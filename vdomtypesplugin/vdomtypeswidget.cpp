33d85ef61fab45f1e7f05aa06c45b35b87aa8445
Dmitry Zakharov <dmitry.zakharov@vdombox.ru>
2017-03-09 10:55:43 +0700

    Draw widget border with dash line.

--- vdomtypesplugin/vdomtypeswidget.cpp
+++ vdomtypesplugin/vdomtypeswidget.cpp
@@ -33,6 +33,8 @@ void VdomTypesWidget::paintEvent(QPaintEvent *)
 //    QTime time = QTime::currentTime();
 
     QPainter painter(this);
+    painter.setPen(Qt::DashLine);
+    painter.drawRect(0, 0, width()-1, height()-1);
     painter.drawText(2, 12, vdomType->displayName);
 //    painter.setRenderHint(QPainter::Antialiasing);
 //    painter.translate(width() / 2, height() / 2);
