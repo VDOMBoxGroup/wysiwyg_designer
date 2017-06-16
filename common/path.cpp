eb3cf7b864ede6465b05de679213c30fedcc6c7b
Dmitry Zakharov <dmitry.zakharov@vdombox.ru>
2017-06-16 15:03:28 +0700

    Move writable locations to a separate folder under temporary path.

--- common/path.cpp
+++ common/path.cpp
@@ -3,8 +3,45 @@
 #include <QFileInfo>
 #include <QDir>
 
-QString defaultPath(const QString &p)
+inline QString JoinPath(const QString &p1, const QString &p2)
+{
+    QString a = p1, b = p2, d = "/";
+    if (a.endsWith(d))
+        a.truncate(a.size() - d.size());
+    if (b.startsWith(d))
+        d = "";
+    return a + d + b;
+}
+
+QString TempDirPath(const QString &p)
+{
+    static QString p1 = JoinPath(QDir::tempPath(), defaultTempDirPath);
+    return JoinPath(p1, p);
+}
+
+void MakeTempPath()
+{
+    QDir().mkpath(TempDirPath(defaultResourcePath));
+    //qDebug(TempDirPath("").toLatin1().constData());
+}
+
+QString CurrentPath()
 {
     static QString appPath = QFileInfo(QCoreApplication::applicationFilePath()).absoluteDir().path();
-    return appPath + p;
+    return appPath;
+}
+
+QString DefaultPath(const QString &p)
+{
+    return JoinPath(CurrentPath(), p);
+}
+
+QString DefaultResourcePath()
+{
+    return TempDirPath(defaultResourcePath);
+}
+
+QString DefaultWidgetsFilePath()
+{
+    return TempDirPath(defaultWidgetsFileName);
 }
--- common/path.h
+++ common/path.h
@@ -4,10 +4,26 @@
 #include <QString>
 
 // default paths relative to the application module directory
+// or working data directory under temporary path
 static const QString defaultWidgetsFileName = "/widgets.xml";
 static const QString defaultPluginPath = "/designer";
+static const QString defaultResourcePath = "/resources";
 static const QString defaultTypesFileName = "/types.xml";
+static const QString defaultTempDirPath = "/vdom/wysiwygeditor";
 
-QString defaultPath(const QString &p);
+// create working data directory under temporary path
+void MakeTempPath();
+
+// return application executable directory
+QString CurrentPath();
+
+// return full path under application executable directory
+QString DefaultPath(const QString &p);
+
+// return resources path under working data directory
+QString DefaultResourcePath();
+
+// return widgets file path under working data directory
+QString DefaultWidgetsFilePath();
 
 #endif // PATH_H
