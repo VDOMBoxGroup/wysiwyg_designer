cfcd2282c5b8b9e414d90e70e8e8eb700388e075
Dmitry Zakharov <dmitry.zakharov@vdombox.ru>
2017-06-16 15:01:24 +0700

    Utility functions.

--- common/util.cpp
+++ common/util.cpp
@@ -1,6 +1,9 @@
 #include "util.h"
+#include <QXmlStreamWriter>
+#include <QBuffer>
+#include <QFile>
 
-QColor parseColor(const QString &s)
+QColor ParseColor(const QString &s)
 {
     QString ss = s;
     if (ss.startsWith("#"))
@@ -9,4 +12,55 @@ QColor parseColor(const QString &s)
     return QColor((color >> 16) & 0xFF,
                   (color >> 8) & 0xFF,
                   color & 0xFF);
+}
+
+QString GetElementXml(QXmlStreamReader &xml)
+{
+    QBuffer buffer;
+    buffer.open(QBuffer::WriteOnly);
+
+    QXmlStreamWriter out(&buffer);
+
+    QString tagName = xml.name().toString();
+    int count = 1;
+
+    while (!xml.atEnd()) {
+        QXmlStreamReader::TokenType token = xml.readNext();
+        //qDebug(xml.name().toString().toLatin1().constData());
+        //qDebug("token %d", (int)token);
+        if (token == QXmlStreamReader::StartElement) {
+            if (!xml.name().compare(tagName))
+                count++;
+            out.writeStartElement(xml.name().toString());
+            out.writeAttributes(xml.attributes());
+        } else if (token == QXmlStreamReader::EndElement) {
+            if (!xml.name().compare(tagName)) {
+                count--;
+                if (!count)
+                    break;
+            }
+            out.writeEndElement();
+        } else if (token == QXmlStreamReader::Characters) {
+            out.writeCharacters(xml.text().toString());
+        }
+    }
+
+    return buffer.buffer();
+}
+
+void WriteFile(const QString &fname, const QByteArray &data)
+{
+    QFile file(fname);
+    if (!file.open(QIODevice::WriteOnly | QIODevice::Text)) {
+        qDebug("Can't open file \'%s\': %s", fname.toLatin1().constData(), file.errorString().toLatin1().constData());
+        return;
+    }
+    file.write(data);
+}
+
+void WriteFile(const QString &fname, const QString &data)
+{
+    QByteArray a;
+    a.append(data);
+    WriteFile(fname, a);
 }
--- common/util.h
+++ common/util.h
@@ -4,8 +4,9 @@
 #include <QColor>
 #include <QString>
 #include <Map>
+#include <QXmlStreamReader>
 
-QColor parseColor(const QString &s);
+QColor ParseColor(const QString &s);
 
 template<typename K, typename V>
 typename QMap<K, V>::const_iterator FindFirst(const QMap<K, V> &m, const V &v)
@@ -15,5 +16,42 @@ typename QMap<K, V>::const_iterator FindFirst(const QMap<K, V> &m, const V &v)
             return i;
     return m.end();
 }
+
+template<typename K, typename V>
+QMap<K, V> operator-(const QMap<K, V> &m1, const QMap<K, V> &m2)
+{
+    QMap<K, V> ret;
+    for (typename QMap<K, V>::const_iterator i=m1.begin(); i!=m1.end(); i++) {
+        typename QMap<K, V>::const_iterator j = m2.find(i.key());
+        if (j == m2.end() || i.value() != j.value())
+            ret.insert(i.key(), i.value());
+    }
+    return ret;
+}
+
+inline bool equals(const QXmlStreamReader &xml, const QString &str)
+{
+    return (xml.name().compare(str, Qt::CaseInsensitive) == 0);
+}
+
+inline QString attr(const QXmlStreamReader &xml, const QString &name)
+{
+    return xml.attributes().value(name).toString();
+}
+
+inline QString get(QXmlStreamReader &xml)
+{
+    return xml.readElementText().trimmed();
+}
+
+inline QString capitalize(const QString &s)
+{
+    return s.left(1).toUpper() + s.mid(1);
+}
+
+QString GetElementXml(QXmlStreamReader &xml);
+
+void WriteFile(const QString &fname, const QByteArray &data);
+void WriteFile(const QString &fname, const QString &data);
 
 #endif // UTIL_H
